97be578b98d39dd671d4052ce3453941b1b224fa
AndrewNalley <104536377+AndrewNalley@users.noreply.github.com>
2023-06-13 19:39:28 -0500

    add inquirer queries

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Runs application using imports from lib folder.
 const inquirer = require('inquirer')
+const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
 const fs = require('fs')
 
 const { Shape } = require('./lib/shapes.js');
@@ -9,27 +10,43 @@ const { Triangle } = require('./lib/shapes.js');
 
 // inquirer prompt
 
-inquirer    
-    .prompt([
-        {
-            type: 'list',
-            message: 'First, choose a shape.',
-            name: 'shape',
-            choices: [
-              { name: 'Triangle', value: Triangle },
-              { name: 'Circle', value: Circle }, 
-              { name: 'Square', value: Square }, 
-            ]
-          },
-        {
-            type: 'input',
-            message: 'What is your project name?',
-            name: 'projectname',
-          }
-    ])
-// takes input and creates svg file with user-provided information
-    .then(({color, shape, shapeColor, text, textColor}) => fs.writeFile("/examples/logo.svg", 
-// display logo
-` `, err => err ? console.log(err) : console.log("Generated logo.svg"))
-);
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      message: 'First, choose a shape.',
+      name: 'shape',
+      choices: [
+        { name: 'Triangle', value: Triangle },
+        { name: 'Circle', value: Circle },
+        { name: 'Square', value: Square },
+      ]
+    },
+    {
+      type: 'input',
+      message: 'Next, what color do you want for the shape?',
+      name: 'shapeColor',
+    },
+    {
+      type: 'maxlength-input',
+      message: 'What acronym do you want for your logo?',
+      name: 'text',
+      maxLength: 3
+    },
+    {
+      type: 'input',
+      message: 'What color do you want for the text?',
+      name: 'textColor',
+    },
+    {
+      type: 'input',
+      message: 'Finally, what color do you want for the background?',
+      name: 'color',
+    },
+  ])
+  // takes input and creates svg file with user-provided information
+  .then(({ shape, shapeColor, text, textColor, color }) => fs.writeFile("/examples/logo.svg",
+    // display logo
+    `const newShape = new shape`, err => err ? console.log(err) : console.log("Generated logo.svg"))
+  );
 // Log error if thrown ^ 
